e9e8da5af63d79a705a4770d6ea0808e402f5457
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-05 02:41:28 +0530

    Populate the data of home page in local storage

--- js/home.js
+++ js/home.js
@@ -1,25 +1,34 @@
+let addressBookList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    addressBookList = getAddressBookDataFromStorage();
     createInnerHtml();
 });
+
+const getAddressBookDataFromStorage = () => {
+    return localStorage.getItem('AddressBookList') ?
+                        JSON.parse(localStorage.getItem('AddressBookList')) : [];
+  }
+
+  
 const createInnerHtml = () => {
     const headerHtml = "<th>Fullname</th> <th>Address</th><th>City</th>" +
                        "<th>State</th> <th>Zip Code</th> <th>Phone Number</th><th></th>";
     let innerHtml =`${headerHtml}`;       
-  //  for(const empPayrollData of empPayrollList) {
+    for(const addressBookData of addressBookList) {
      innerHtml = `${innerHtml}        
     <tr>
-        <td>Varaza Mishra</td>
-        <td>Marve Road, Next To Manirastna Malad(West)</td>
-        <td>Mumbai</td>
-        <td>Maharashtra</td>
-        <td>400064</td>
-        <td>02228017752</td>
+        <td>${addressBookData._name}</td>
+        <td>${addressBookData._address}</td>
+        <td>${addressBookData._city}</td>
+        <td>${addressBookData._state}</td>
+        <td>${addressBookData._zipCode}</td>
+        <td>${addressBookData._phoneNumber}</td>
         <td>
-        <img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.PNG" alt="delete">
-        <img id="1" onclick="update(this)" src="../assets/icons/create-black-18dp.PNG" alt="edit">
+        <img id="${addressBookData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.PNG" alt="delete">
+        <img id="${addressBookData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.PNG" alt="edit">
         </td>
     </tr>
     `;
-   // }
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
   }
