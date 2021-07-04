0da00f8a0ebc7b774ae686c9402662ffa6e437c2
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-05 05:21:23 +0530

    added some properties like local storage is enable or disable and added server url

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,7 @@
 let site_properties = {
+    use_local_storage: "false",
     home_page: "../pages/home.html",
-    add_address_Book_page: "../pages/address_form.html"
+    add_address_Book_page: "../pages/address_form.html",
+    server_url: "http://127.0.0.1:3000/AddressBookDB/"
 }
 
